735f083ac4fcb456dbecdd2f33f04fa6ad8b8f59
raidenz <th.deny@gmail.com>
2013-06-20 08:34:02 +0700

    debug

--- index.php
+++ index.php
@@ -35,15 +35,18 @@ Created By George Baker @ http://themeforest.net/user/phoenix - http://twitter.c
 
 ## get current theme name
 
+$current_theme = '';
+if(isset($_GET['theme'])) {
 $current_theme = $_GET['theme'];
+}
 $theme_found = false;
 
 ## build theme data array
 
-$theme_array = array (
+/* $theme_array = array (
 
-array ("id" => "adventure",
-       "url" => "http://labs.burntfeathers.com/adventure",
+array ("id" => "digitalic",
+       "url" => "http://digitalic.wpbootstrap.net",
 	   "themeforest" => "http://themeforest.net/item/adventure-a-unique-photography-wordpress-theme/155571?ref=USERNAME"
 	  ),
 array ("id" => "attique",
@@ -51,12 +54,46 @@ array ("id" => "attique",
 	   "themeforest" => "http://themeforest.net/item/attique-a-stylish-multi-purpose-wordpress-theme/168724"
 	  )
 
-);
+); */
 
+//Theme Array
+$theme_array = array (
+ 
+array (
+"id" => "Agility",
+'type'	=> "Responsive HTML5",
+"url" => "http://agility-html.sevenspark.com",
+"themeforest" => "http://themeforest.net/item/agility-responsive-minimal-html5/1054063",
+"preview"	=> "agility_html.jpg",
+"mobile_redirect" => true,
+),
+'UberMenu'	=> array(
+"id" => "UberMenu - Mega Menu",
+"type"	=> "WP Plugin",
+"url" => "http://wpmegamenu.com",
+"themeforest" => "http://codecanyon.net/item/ubermenu-wordpress-mega-menu-plugin/154703",
+"preview"	=> "http://0.s3.envato.com/files/1967560/megamenupreview.1.1.2.jpg"
+),
+'ScrollCheckpoint' => array(
+"id" => "Scroll Checkpoint",
+"type"	=> "WP Plugin",
+"url" => "http://scrollcheckpoint.sevenspark.com",
+"themeforest" => "http://codecanyon.net/item/scroll-checkpoint-animated-content-panel-plugin/234882",
+"preview"	=> "scroll_checkpoint.jpg",
+),
+'MenuManager'	=> array(
+"id" => "Menu Management Enhancer",
+"type"	=> "WP Plugin",
+"url" => "http://menumanager.sevenspark.com",
+"themeforest" => "http://codecanyon.net/item/menu-management-enhancer-for-wordpress/529353",
+"preview"	=> "menu_manager.jpg",
+),
+);
+$redirect = '';
 if (!$redirect) :
 
 ## get current theme data
-
+//var_dump($theme_array);
 foreach ($theme_array as $i => $theme) :
 
 	if ($theme['id'] == $current_theme) :
@@ -71,11 +108,18 @@ foreach ($theme_array as $i => $theme) :
 
 endforeach;
 
-if ($theme_found == false) :
+/* if ($theme_found == false) :
 
 	$current_theme_name = $theme_array[0]['id'];
 	$current_theme_url = $theme_array[0]['url'];
 	$current_theme_purchase_url = $theme_array[0]['themeforest'];	
+
+endif; */
+if ($theme_found == false) :
+
+	$current_theme_name = ucfirst($theme['id']);
+	$current_theme_url = ucfirst($theme['url']);
+	$current_theme_purchase_url = $theme['themeforest'];	
 
 endif;
 
@@ -220,10 +264,10 @@ endif;
         	
         	return false;
 			//fix remove frame open current theme location
-			$('.remove_frame').delegate('a','click',bind(function(){
+			/* $('.remove_frame').delegate('a','click',bind(function(){
 				top.location.href=$('#iframe')[0].contentWindow.location.href;
 				return false;
-			});
+			}); */
         	
         	});
         
@@ -249,10 +293,24 @@ endif;
 			
 				<?php
 			
-				foreach ($theme_array as $i => $theme) :
+				/* foreach ($theme_array as $i => $theme) :
 				
 				echo '<li><a href="#" rel="' . $theme['url'] . ',' . $theme['themeforest'] . '">' . ucfirst($theme['id']) . '</a></li>';
 				
+				endforeach; */
+				foreach ($theme_array as $i => $theme) :
+					echo '<li><a href="#" rel="' . $theme['url'] . ',' . $theme['themeforest'] . '?ref=sevenspark">' .
+					ucfirst($theme['id']) .
+					' <span class="product-type">'.$theme['type'].'</span>';
+					if(isset($theme['preview'])){
+					echo '<span class="product-preview"><img src="';
+					if(strpos($theme['preview'], 'http://') === false){
+					echo 'product_previews/'.$theme['preview'];
+					}
+					else echo $theme['preview'];
+					echo '" /></span>';
+					}
+					echo '</a></li>';
 				endforeach;
 				
 				?>
